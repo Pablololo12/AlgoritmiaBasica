33923c1a6a17dff23c4a2f5e4975ff84e3b1c4c8
Mario Arcega <mario_arcega@hotmail.com>
2016-03-03 14:03:27 +0100

    añadido algoritmo voraz de huffman y modificado el heap para arboles

---
+++ Huffman/huffman.c
@@ -0,0 +1,36 @@
+#include "tipos.h"
+#include "heap.h"
+
+struct arbol * huffman(long frecuencia[256]){
+
+	struct * heap = iniciar_heap();
+
+	int i;
+	for (i = 0; i<256; i++){
+		if (frecuencia[i] != 0){
+			struct arbol * arbol = malloc(sizeof(struct arbol));
+			arbol -> elemento = i;
+			arbol -> apariciones = frecuencia[i];
+			arbol -> hijo_i = NULL;
+			arbol -> hijo_d = NULL;
+			heap = insertar_heap(heap, arbol);
+		}
+	}
+
+	struct arbol * arbolx;
+	struct arbol * arboly;
+	while ((heap -> tamanyo) > 1){
+		arbolx = borrar_heap(heap);
+		arboly = borrar_heap(heap);
+			
+		struct arbol * arbolz = malloc(sizeof(struct arbol));
+		arbolz -> apariciones = (arbolx -> apariciones) + (arboly -> apariciones);
+		arbolz -> hijo_i = arbolx;
+		arbolz -> hijo_d = arboly;
+		heap = insertar_heap(heap, arbolz);
+	}
+
+	arbolx = borrar_heap(heap);
+	free(heap);
+	return arbolx;
+}
---
+++ Huffman/huffman.h
@@ -0,0 +1,6 @@
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+struct arbol * huffman(long[256]);
+
+#endif
