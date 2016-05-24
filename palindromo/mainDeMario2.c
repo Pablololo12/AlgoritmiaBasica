68092e460aa73898a6379e8810a05f5554829eb5
MarioArcega <mario_arcega@hotmail.com>
2016-05-24 11:56:47 +0200

    Add files via upload

--- palindromo/mainDeMario2.c
+++ palindromo/mainDeMario2.c
@@ -13,51 +13,9 @@ char* insertar (char *string, int pos, char c){
     return aux;
 }
 
-int main(int argc, char **argv)
-{
-    int s = strlen(argv[1]);
-	char *string = (char *) malloc(sizeof(char) * s);
-	strcpy(string, argv[1]);
-	
-	unsigned int C[s][s];// = (unsigned int [][]) malloc(s * s * sizeof(unsigned int));
-	unsigned char camino[s][s];
-	
-	int i = s - 1;
-	for (; i >= 0; i--){
-	    C[i][i] = 0;
-	    int j = i + 1;
-	    for (; j < s ; j++){
-	        unsigned int min = C[i+1][j-1];
-	        camino[i][j] = 0;
-            //C(i,j) = min (C(i+1, j-1), 1 + C(i+1, j), 1 + C(i, j-1))
-	        if (string[i] != string[j]){ //los caracteres de los extremos se intercambian
-	            min++;
-	            camino[i][j] = 1;
-	            //printf("El caracter %d (%c) se cambia por %c\n", j, string[j], string[i]);
-	        }
-	        if (min > 1 + C[i+1][j]){ //se añade una caracter igual al extremo izquierdo en la derecha
-	            min = 1 + C[i+1][j];
-	            camino[i][j] = 2;
-	            //printf("Se añade el caracter %c despues del caracter %d (%c)\n", string[i], j, string[j]);
-	        }  else if (min > 1 + C[i][j-1]){//se añade una caracter igual al extremo derecho en la izquierda
-	            min = 1 + C[i][j-1];
-	            camino[i][j] = 3;
-	            //printf("Se añade el caracter %c antes del caracter %d (%c)\n", string[j], i, string[i]);
-	        }
-	        C[i][j] = min;
-	       /** printf("\ncoste %d en c[%d][%d] con camino %d\n", C[i][j], i, j, camino[i][j]);
-	        printf("coste camino 0: %d\n", C[i+1][j+1]);
-	        printf("coste camino 1: %d\n", C[i+1][j+1] + 1);
-	        printf("coste camino 2: %d\n", C[i+1][j] + 1);
-	        printf("coste camino 0: %d\n", C[i][j-1] + 1);*/
-	        //printf("\n\n");
-	    }
-	}
-	
-	int j;
-	
-	
-	printf("Matriz de coste: \n");
+void imprimirMatrices (unsigned int **C, int s){
+    int j, i;
+    printf("Matriz de coste: \n");
 	printf("     ");
 	for (j = 0; j < s; j++){
 	    printf(" %4d", j);
@@ -95,35 +53,79 @@ int main(int argc, char **argv)
 	    printf(" %3d|", i);
 	    for (j = 0; j < s; j++){
 	        if (j > i){
-	            printf(" %4d", camino[i][j]);
+	            printf(" %4d", C[j][i]);
 	        } else {
 	            printf(" %4d", 0);
 	        }
 	    }
 	    printf("\n");
 	}
+}
+
+int main(int argc, char **argv)
+{
+    int s = strlen(argv[1]);
+	char *string = (char *) malloc(sizeof(char) * s);
+	strcpy(string, argv[1]);
 	
+	unsigned int **C = malloc(sizeof(unsigned int *) * s);
+	int i;
+	for(i=0; i<s; i++) C[i] = calloc(s ,sizeof(unsigned int));
+	//C[i][j] es el coste de hacer palindromo la cadena string(i...j)
+	//C[j][i] es el camino óptimo
+	
+	i = s - 1;
+	for (; i >= 0; i--){
+	    C[i][i] = 0;
+	    int j = i + 1;
+	    for (; j < s ; j++){
+	        unsigned int min;
+	        if (i+1 > j+1){
+	            min = 1 + C[i+1][j];
+	            C[j][i] = 2;
+	        } else { //Si C[i+1][j-1] tiene sentido
+	            min = C[i+1][j-1];
+	            C[j][i] = 0;
+	            if (string[i] != string[j]){ //los caracteres de los extremos se intercambian
+    	            min++;
+    	            C[j][i] = 1;
+    	        }
+	        }
+	        if (min > 1 + C[i+1][j]){ //se añade una caracter igual al extremo izquierdo en la derecha
+	            min = 1 + C[i+1][j];
+	            C[j][i] = 2;
+	        }  else if (min > 1 + C[i][j-1]){//se añade una caracter igual al extremo derecho en la izquierda
+	            min = 1 + C[i][j-1];
+	            C[j][i] = 3;
+	        }
+	        C[i][j] = min;
+	    }
+	}
+	
+	//imprimirMatrices(C, s);
+	
+	int j;
 	i = 0;
 	j = s-1;
 	int prefijos = 0;
 	printf("\nNúmero minimo: %d\n", C[i][j]);
 	printf("Cadena original -> %s\n", string);
-	while (i != j){
-	    if (camino[i][j] == 0){
+	while (i < j){
+	    if (C[j][i] == 0){
 	        i++;
 	        j--;
-	    } else if (camino[i][j] == 1){
+	    } else if (C[j][i] == 1){
 	        string[j + prefijos] = string[i + prefijos];
 	        printf("Cambiar el caracter %d por %c -> %s\n", j + 1 + prefijos, string[j + prefijos], string);
 	        i++;
 	        j--;
-	    } else if (camino[i][j] == 2){
+	    } else if (C[j][i] == 2){
 	        char *aux = insertar(string, j+1, string[i]);
 	        free(string);
 	        string = aux;
 	        printf("Añadir caracter %c en pos %d -> %s\n", string[i+prefijos], j + 1 + prefijos, string);
 	        i++;
-	    } else if (camino[i][j] == 3){
+	    } else if (C[j][i] == 3){
 	        char *aux = insertar(string, i + prefijos, string[j]);
 	        free(string);
 	        string = aux;
@@ -133,12 +135,3 @@ int main(int argc, char **argv)
 	    }
 	}
 }
-/**
-C(i,j) = min (C(i+1, j-1), 1 + C(i+1, j), 1 + C(i, j-1))
-
-0123
-x012
-xx01
-xxx0
-alohola
-*/
